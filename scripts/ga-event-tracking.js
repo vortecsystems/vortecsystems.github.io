6d7ab586020e0b15a357f79a80aff647b3ddf213
Tom Smith <tom@tomesmith.co.uk>
2020-06-07 13:09:44 +0100

    Remove old event tracking
    Remove old JQuery event tracking - will re-do this via tag manager

--- scripts/ga-event-tracking.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    jQuery('a[href$=".pdf"]').click(function () {
-        ga('send', 'event', 'Download Literature', 'Click', this.title);
-    });
-    jQuery('a[href^="mailto:"]').click(function () {
-        ga('send', 'event', 'Click Email Link', 'ClickMailLink', this.href);
-    });
-});
